279504edc8ee5980b2b3e8afae182054f4a61796
OLIVER LOERTSCHER <loertscheroliver@gmail.com>
2017-02-07 11:03:39 -0500

    #024: split styles

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,17 @@ gulp.task('sass', function() {
 		
 })
 
+gulp.task('sassTop', function() {
+	gulp.src('./src/sass/top.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename('styletop.min.css'))
+	.pipe(gulp.dest('./dist/'))
+	.pipe(cssMin())
+	.pipe(uglifycss());
+		
+		
+})
+
 gulp.task('default', function() {
 	gulp.src('./')
 	gulp.watch('./src/sass/*.scss', ['sass'])
